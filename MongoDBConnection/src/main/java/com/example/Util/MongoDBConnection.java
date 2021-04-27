33dbbd55c68b8398dedd29f5e48dfddad65cc639
Dengyuesheng <705501518@qq.com>
2021-04-27 17:52:58 +0800

    creatInsert-failed

---
+++ MongoDBConnection/src/main/java/com/example/Util/MongoDBConnection.java
@@ -0,0 +1,21 @@
+package com.example.Util;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class MongoDBConnection {
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    public void ConnectionTest() {
+        Map<Object, Object> map = new HashMap<>();
+        map.put("name", "pingguo");
+        mongoTemplate.insert(map);
+    }
+}
--- MongoDBConnection/src/test/java/com/example/DemoApplicationTests.java
+++ MongoDBConnection/src/test/java/com/example/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example;
 
+import com.example.Util.MongoDBConnection;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
+    @Autowired
+    MongoDBConnection mongoDBConnection;
 
     @Test
     void contextLoads() {
+        mongoDBConnection.ConnectionTest();
     }
 
 }
