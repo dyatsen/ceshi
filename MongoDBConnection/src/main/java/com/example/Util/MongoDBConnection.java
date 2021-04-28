aaa067aaa614b3a7899a001ef0814a294f075eab
Dengyuesheng <705501518@qq.com>
2021-04-28 08:28:56 +0800

    pingguo to xueli

--- MongoDBConnection/src/main/java/com/example/Util/MongoDBConnection.java
+++ MongoDBConnection/src/main/java/com/example/Util/MongoDBConnection.java
@@ -15,7 +15,7 @@ public class MongoDBConnection {
 
     public void ConnectionTest() {
         Map<Object, Object> map = new HashMap<>();
-        map.put("name", "pingguo");
+        map.put("name", "xueli");
         mongoTemplate.insert(map);
     }
 }
